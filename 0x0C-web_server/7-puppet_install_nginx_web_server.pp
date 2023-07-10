002722e64af607c0fd96e1edcc24e8951435b218
meseret1918 <meseret1918@gmail.com>
2023-07-10 03:22:04 -0700

    web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 # Puppet manifest that installs and configures an Nginx server with a 301 redirect
 
-package { 'nginx':
-ensure => installed,
-}
-file_line { 'install':,
-ensure => 'present',
-path => '/etc/nginx/sites-enabled/default',
-after => 'listen 80 default_server;',
-line => 'rewrite ^ /redirect_me https://www.github.com/meseret1918 permanent;',
-}
-file {'/var/www/html/index.html':
-content => 'Hello World!',
-}
-service {'nginx':
-ensure => running,
-required => package{'nginx'},
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/meseret1918 permanent;/" /etc/nginx/sites-enabled/default ; sudo service nginx start',
 }
