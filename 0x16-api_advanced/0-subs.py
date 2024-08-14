c89385e74a77a4fcb037a7f409d43cf4b63af8a3
meseret1918 <meseret1918@gmail.com>
2024-08-14 09:21:04 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""API a function that queries the Reddit API and returns
+the number of subscribers (total subscribers) for a given
+subreddit. If an invalid
+subreddit is given, the function should return 0
 """
-0-main
-"""
-import sys
+import requests
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
+def number_of_subscribers(subreddit):
+    """Get number of subsribers for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 404:
+        subs = response.json().get("data").get("subscribers")
+        return subs
+    return 0
